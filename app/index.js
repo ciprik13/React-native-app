df089f5259f1a5d90bf51317052a6c24bf8d719d
ciprik13 <ciprianmoisenco.it@gmail.com>
2024-12-20 21:31:24 +0200

    Initial commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import { Text } from 'react-native';
+
+
+const Home = () => {
+    return (
+        <View>
+            <Text>Welcome to the Home Screen!</Text>
+        </View>
+    )
+}
+
+export default Home;
